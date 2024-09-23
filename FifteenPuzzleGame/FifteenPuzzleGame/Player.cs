7d87967cebeb7d26f50d7ac613d01f1a3c547885
Joshua DiChello <jddichello@gmail.com>
2024-09-23 15:33:07 -0500

    added player class, properties, and constructor

---
+++ FifteenPuzzleGame/FifteenPuzzleGame/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FifteenPuzzleGame
+{
+    public class Player
+    {
+        private int _x;
+        private int _y;
+
+        public int X 
+        {
+            get { return _x; } 
+            set { _x = value; }
+        }
+        public int Y 
+        {
+            get { return _y; } 
+            set { _y = value; }
+        }
+
+        public int MoveCount { get; } = 0;
+
+        public Player(int x, int y, int moveCount)
+        {
+            X = x;
+            Y = y;
+            MoveCount = moveCount;
+        }
+    }
+}
