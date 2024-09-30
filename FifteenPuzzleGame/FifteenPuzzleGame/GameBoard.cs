cd5b97249bb927cf6173202c47ffd143c073f029
Joshua DiChello <jddichello@gmail.com>
2024-09-30 13:18:35 -0500

    renamed properties, added board property, and removed conflicting method params

--- FifteenPuzzleGame/FifteenPuzzleGame/GameBoard.cs
+++ FifteenPuzzleGame/FifteenPuzzleGame/GameBoard.cs
@@ -9,24 +9,30 @@ namespace FifteenPuzzleGame
     public class GameBoard
     {
         private static Random randNum = new Random();
-        private static List<int> numberPool = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 , 13, 14, 15, 0 }; // this needs uncoupled
-
-        private int[,] _gameBoard;
+        private static List<int> numberPool = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 , 13, 14, 15, 0 }; 
+        //uncouple this list, and make a property for gameboard size
+        //then use a forloop to populate the list with numbers based on gameboard size
+        private int[,] _board;
+        public int[,] Board 
+        {
+            get { return _board; }
+            set { _board = value; }
+        }
 
         public GameBoard()
         {
-            _gameBoard = new int[4, 4]; //todo: need to uncouple this for gameboard sizes by user choice
-            InitializeValues(_gameBoard);
+            _board = new int[4, 4]; //todo: need to uncouple this for gameboard sizes by user choice
+            InitializeValues(_board);
         }
 
-        private static void InitializeValues(int[,] board)
+        private static void InitializeValues(int[,] gameBoard)
         {
 
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
-                    board[i,j] = PopulateAndRemove(numberPool);
+                    gameBoard[i,j] = PopulateAndRemove(numberPool);
                 }
             }
         }
