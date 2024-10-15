23d505ffdf800609472a730ad96ea900c4b29804
Joshua DiChello <jddichello@gmail.com>
2024-10-15 14:46:44 -0500

    added props, player location found and stored on creation

--- FifteenPuzzleGame/FifteenPuzzleGame/Player.cs
+++ FifteenPuzzleGame/FifteenPuzzleGame/Player.cs
@@ -6,6 +6,8 @@ namespace FifteenPuzzleGame
     {
         private int _x;
         private int _y;
+        private int _moveCount;
+        private GameBoard _gameBoard;
 
         public int X 
         {
@@ -18,12 +20,41 @@ namespace FifteenPuzzleGame
             set { _y = value; }
         }
 
-        public int MoveCount { get; } = 0;
-
-        public Player(int x, int y)
+        public int MoveCount
         {
-            X = x;
-            Y = y;
+            get { return _moveCount; }
+        }
+
+        public Player(int x, int y, GameBoard gameBoard)
+        {
+            _gameBoard = gameBoard;
+            _x = GetAxisLocation("x");
+            _y = GetAxisLocation("y");
+            _moveCount = 0;
+        }
+
+        private int GetAxisLocation(string axis)
+        {
+            int axisLocation = -1;
+
+            for (int i = 0; i < _gameBoard.BoardSize; i++)
+            {
+                for (int j = 0; j < _gameBoard.BoardSize; j++)
+                {
+                    if (_gameBoard.Board[i, j] == 0)
+                    {
+                        if (axis == "x")
+                        {
+                            axisLocation = i;
+                        }
+                        else if (axis == "y")
+                        {
+                            axisLocation = j;
+                        }
+                    }
+                }
+            }
+            return axisLocation;
         }
     }
 }
