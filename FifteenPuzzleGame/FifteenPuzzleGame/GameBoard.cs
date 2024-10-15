a10abb7cdfa11d812278d8ef39718af0407af354
Joshua DiChello <jddichello@gmail.com>
2024-10-15 14:45:21 -0500

    gameboard will now always create a solvable puzzle

--- FifteenPuzzleGame/FifteenPuzzleGame/GameBoard.cs
+++ FifteenPuzzleGame/FifteenPuzzleGame/GameBoard.cs
@@ -8,13 +8,11 @@ namespace FifteenPuzzleGame
 {
     public class GameBoard
     {
-        private static List<int> numberPool = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 , 13, 14, 15, 0 };
-        //uncouple this list, and make a property for gameboard size
-        //then use a forloop to populate the list with numbers based on gameboard size
         private const int DEFAULT_BOARD_SIZE = 3;
 
+        private List<int> _numberPool;
         private Random _randomNumber;
-        private char[,] _board;
+        private int[,] _board;
         private int _boardSize;
 
         public int BoardSize 
@@ -22,17 +20,17 @@ namespace FifteenPuzzleGame
             get { return _boardSize; } 
             set 
             {
-                if (_boardSize < 3)
+                if (value >= 3)
                 {
                     _boardSize = value;
                 }
                 else
                 {
-                    _boardSize = DEFAULT_BOARD_SIZE;
+                    _boardSize = value >= 3 ? value : DEFAULT_BOARD_SIZE;
                 }
             }
         }
-        public char[,] Board 
+        public int[,] Board 
         {
             get { return _board; }
             set { _board = value; }
@@ -40,45 +38,119 @@ namespace FifteenPuzzleGame
 
         public GameBoard()
         {
-            this._randomNumber = new Random();
-            _board = new char[_boardSize, _boardSize];
-            InitializeValues(_board, _randomNumber);
-
-            /*
-             *                      -----Steps to check if board is solvable before setting it-----
-             * 1. Need to initialize values of gameboard based on user choice (use board size and do n * n and add the space)
-             * 
-             * 2. loop through to populate list to use populate and remove method
-             * 
-             * 3. need to pass the game board into a method that returns bool
-             * 
-             * 4. then the array will need flattened to check if inversion count is even or odd
-             * 
-             * 5. locate the row of the empty space to check if its even or odd (note: even or odd is based from bottom row number up)
-             * 
-             * 6. if not solvable the loop will continue to run and check until a solvable board is created and set
-             */
+            _randomNumber = new Random();
+            _numberPool = new List<int>();
+            BoardSize = DEFAULT_BOARD_SIZE;
+            PopulateRandomNumberPool();
+            do
+            {
+                _board = new int[_boardSize, _boardSize];
+            } while (!IsSolvableBoard(_numberPool, _board, _randomNumber));
         }
 
+        private bool IsSolvableBoard(List<int> numberPool, int[,] gameBoard, Random rndNum)
+        {
+            // Initialize the game board with random values
+            InitializeValues(numberPool,gameBoard, rndNum);
+            // Flatten the 2D array for inversion checking
+            int[] flatBoard = Flatten2DArray(gameBoard);
+            // Check for even number of inversions
+            bool isEvenInversion = IsEvenInversionCount(flatBoard);
+            // Check the row location of the blank space, counting from the bottom
+            bool isEvenFromBottom = CheckRowLocation();
 
-        private static void InitializeValues(char[,] gameBoard, Random rndNum)
+            return CheckSolvability(isEvenInversion, isEvenFromBottom);
+        }
+
+        private bool CheckSolvability(bool isEvenInversion, bool isEvenFromBottom)
+        {
+            if (_boardSize % 2 != 0)
+            {
+                return isEvenInversion; //if board size is odd, then if it will be solvable if inversions are even
+            }
+            else
+            {
+                return (isEvenFromBottom && !isEvenInversion) || (!isEvenFromBottom && isEvenInversion);
+            }
+        }
+        private int FindBlankSpace()
+        {
+            for (int i = _boardSize - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < _boardSize; j++)
+                {
+                    if (_board[i, j] == 0)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+        private bool CheckRowLocation()
+        {
+            int xLocation = FindBlankSpace();
+            return xLocation % 2 == 0; //true if even from bottom, false if odd from bottom
+        }
+        private bool IsEvenInversionCount(int[] flatArr)
+        {
+            int inversionCount = 0;
+
+            for (int i = 0; i < _boardSize * _boardSize - 1; i++)
+            {
+                for (int j = i + 1; j < _boardSize * _boardSize; j++)
+                {
+                    if ((flatArr[j] != 0 && flatArr[i] != 0) && (flatArr[i] > flatArr[j]))
+                    {
+                        inversionCount++;
+                    }
+                }
+            }
+            return inversionCount % 2 == 0;
+        }
+        private int[] Flatten2DArray(int[,] board)
+        {
+            int totalLength = board.Length;
+
+            int[] flatArr = new int[totalLength];
+            int index = 0;
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    flatArr[index++] = board[i, j];
+                }
+            }
+
+            return flatArr;
+        }
+
+        private void InitializeValues(List<int> numberPool,int[,] gameBoard, Random rndNum)
         {
 
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < gameBoard.GetLength(0); i++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < gameBoard.GetLength(1); j++)
                 {
                     gameBoard[i,j] = PopulateAndRemove(numberPool, rndNum);
                 }
             }
         }
-        private static char PopulateAndRemove(List<int> numPool, Random rndNum) //list parameter will go away and use board size
+        private int PopulateAndRemove(List<int> numberPool, Random rndNum)
         {
-            int accessIndex = rndNum.Next(0,numPool.Count);
+            int accessIndex = rndNum.Next(0,numberPool.Count);
             int result = numberPool[accessIndex];
             numberPool.RemoveAt(accessIndex);
 
-            return Convert.ToChar(result);
+            return result;
+        }
+        private void PopulateRandomNumberPool()
+        {
+            for(int i = 0; i < _boardSize * _boardSize; i++)
+            {
+                _numberPool.Add(i);
+            }
         }
     }
 }
