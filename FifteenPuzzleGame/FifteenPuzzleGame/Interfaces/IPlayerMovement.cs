eb2f9e88287efe1710cce2e5c52d671b19cefb59
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2024-11-19 13:05:53 -0500

    implemented interface for moving blank around game board

---
+++ FifteenPuzzleGame/FifteenPuzzleGame/Interfaces/IPlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenPuzzleGame.Interfaces
+{
+    public interface IPlayerMovement
+    {
+        int MoveUp(ConsoleKeyInfo keyPress);
+        int MoveDown(ConsoleKeyInfo keyPress);
+        int MoveLeft(ConsoleKeyInfo keyPress);
+        int MoveRight(ConsoleKeyInfo keyPress);
+    }
+}
