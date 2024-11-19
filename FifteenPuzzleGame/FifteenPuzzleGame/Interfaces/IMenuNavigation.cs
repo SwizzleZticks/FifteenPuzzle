a17e45cbd98388d57f0eab2b4eee51fb202c6470
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2024-11-19 13:05:31 -0500

    implemented menu nav interface for interactive menu

---
+++ FifteenPuzzleGame/FifteenPuzzleGame/Interfaces/IMenuNavigation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenPuzzleGame.Interfaces
+{
+    public interface IMenuNavigation
+    {
+        void MoveUp(ConsoleKeyInfo keyPress);
+        void MoveDown(ConsoleKeyInfo keyPress);
+        void Select(ConsoleKeyInfo keyPress);
+    }
+}
