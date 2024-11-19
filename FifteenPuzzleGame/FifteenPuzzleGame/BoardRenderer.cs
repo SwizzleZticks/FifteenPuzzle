1e86330824dd43fe4b576050b6fe2e5a2193dbf1
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2024-11-19 13:02:55 -0500

    implemented board renderer logic

---
+++ FifteenPuzzleGame/FifteenPuzzleGame/BoardRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifteenPuzzleGame
+{
+    public class BoardRenderer
+    {
+        public void RenderBoard(GameBoard board)
+        {
+            Console.WriteLine(PrintColumns(board));
+            for (int i = 0; i < board.BoardSize; i++)
+            {
+                for (int j = 0; j < board.BoardSize; j++)
+                {
+                    Console.Write("| {0,2} ", board.Board[i,j]);
+                }
+                Console.WriteLine("|");
+                Console.WriteLine(PrintColumns(board));
+            }
+        }
+
+        private string PrintColumns(GameBoard board)
+        {
+            string gridPattern = "+";
+
+            for (int i = 0; i < board.BoardSize; i++)
+            {
+                gridPattern += "----+";
+            }
+            return gridPattern;
+        }
+    }
+}
