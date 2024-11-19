4d6f53e940723cfc803cf6f969f55e9b8c8fd898
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2024-11-19 13:04:58 -0500

    begun game menu logic

---
+++ FifteenPuzzleGame/FifteenPuzzleGame/GameMenu.cs
@@ -0,0 +1,44 @@
+﻿
+using FifteenPuzzleGame.Interfaces;
+
+namespace FifteenPuzzleGame
+{
+    public class GameMenu : IMenuNavigation
+    {
+        private ConsoleKeyInfo _keyPress;
+        private int _boardSize;
+
+        public int BoardSize 
+        { 
+            get {  return _boardSize; }
+            set { _boardSize = value; }
+        }
+
+        public GameMenu(int boardSize)
+        {
+            _boardSize = boardSize;
+        }
+
+        public GameMenu() { }
+
+        public void DisplayMenu()
+        {
+
+        }
+
+        public void MoveUp(ConsoleKeyInfo keyPress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveDown(ConsoleKeyInfo keyPress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Select(ConsoleKeyInfo keyPress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
