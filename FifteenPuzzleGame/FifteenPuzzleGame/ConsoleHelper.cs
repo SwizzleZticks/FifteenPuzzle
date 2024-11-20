4fb0bc9b62ab6d9d4d9beeb06275c6dca576e7b9
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2024-11-19 20:07:25 -0500

    added class to center text in console window

---
+++ FifteenPuzzleGame/FifteenPuzzleGame/ConsoleHelper.cs
@@ -0,0 +1,32 @@
+﻿
+namespace FifteenPuzzleGame
+{
+    public static class ConsoleHelper
+    {
+        public static void WriteLineCentered(string text)
+        {
+            int consoleWidth = Console.WindowWidth;
+
+            int textLength = text.Length;
+            int startPositionX = (consoleWidth / 2) - (textLength / 2);
+
+            
+
+            Console.SetCursorPosition(startPositionX, Console.CursorTop);
+            Console.WriteLine(text);
+        }
+
+        public static void WriteCentered(string text)
+        {
+            int consoleWidth = Console.WindowWidth;
+
+            int textLength = text.Length;
+            int startPositionX = (consoleWidth / 2) - (textLength / 2);
+
+
+
+            Console.SetCursorPosition(startPositionX, Console.CursorTop);
+            Console.Write(text);
+        }
+    }
+}
