eb52a40f370cd0027d9d513da97189b8aa55a30b
Joshua DiChello <48339347+SwizzleZticks@users.noreply.github.com>
2024-11-22 16:31:53 -0500

    competed logic to check is puzzle is solved, changed colors, and board field name

--- FifteenPuzzleGame/FifteenPuzzleGame/Game.cs
+++ FifteenPuzzleGame/FifteenPuzzleGame/Game.cs
@@ -3,30 +3,94 @@ namespace FifteenPuzzleGame
 {
     public class Game
     {
-        private GameBoard _board;
+        private GameBoard _gameBoard;
         private GameMenu _menu;
-        private BoardRenderer _boardRenderer;
+        private BoardRenderer _gameBoardRenderer;
         private Player _player;
 
         public Game()
         {
-            _boardRenderer = new BoardRenderer();
+            _gameBoardRenderer = new BoardRenderer();
             _menu = new GameMenu();
-            _board = new GameBoard(_menu.GetGameSize());
-            _player = new Player(_board);
+            _gameBoard = new GameBoard(_menu.GetGameSize());
+            _player = new Player(_gameBoard);
 
         }
         public void Run()
         {
             Console.Title = "15-Puzzle Game";
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.Blue;
 
-            while (true)
+            while (!IsCompletedPuzzle())
             {
                 Console.Clear();
-                _boardRenderer.RenderBoard(_board, _player);
+                _gameBoardRenderer.RenderBoard(_gameBoard, _player);
                 _player.Move();
             }
+            Console.Clear();
+            _gameBoardRenderer.RenderBoard(_gameBoard,_player);
+            PrintComplete();       
+        }
+        public static bool IsPlayingAgain()
+        {
+            bool isActiveLoop = true;
+            bool isPlaying = false;
+
+            do
+            {
+                ConsoleHelper.WriteLineCentered("Would you like to continue playing (Y/N)?");
+                string userInput = Console.ReadLine().ToUpper();
+
+                if (userInput == "Y")
+                {
+                    isPlaying = true;
+                    isActiveLoop = false;
+                }
+                else if (userInput == "N")
+                {
+                    isPlaying= false;
+                    isActiveLoop = false;
+                }
+            }while (isActiveLoop);
+
+            return isPlaying;
+        }
+        private void PrintComplete()
+        {
+            ConsoleHelper.WriteLineCentered("*****************", ConsoleColor.Green);
+            ConsoleHelper.WriteLineCentered("*  [COMPLETE!]  *", ConsoleColor.Green);
+            ConsoleHelper.WriteLineCentered("*****************", ConsoleColor.Green);
+
+            ConsoleHelper.WriteLineCentered("Press enter to exit...", ConsoleColor.Green);
+            Console.ReadLine();
+        }
+
+        private bool IsCompletedPuzzle()
+        {
+            int count = 1;
+
+            for (int i = 0; i < _gameBoard.Board.GetLength(0); i++)
+            {
+                for (int j = 0; j < _gameBoard.Board.GetLength(1); j++)
+                {
+                    if (i == _gameBoard.Board.GetLength(0) - 1 && j == _gameBoard.Board.GetLength(1) - 1)
+                    {
+                        if (_gameBoard.Board[i, j] != 0)
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        if (_gameBoard.Board[i, j] != count)
+                        {
+                            return false;
+                        }
+                        count++;
+                    }
+                }
+            }
+            return true;
         }
     }
 }
